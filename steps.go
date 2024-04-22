bd2bbb2951d2597cd657e509715cb6767c4be98d
Stefan Claas <stefan.claas@mail.ru>
2024-04-22 18:05:18 +0200

    Add files via upload

--- steps.go
+++ steps.go
@@ -1,141 +1,135 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "math/big"
-    "os"
+	"flag"
+	"fmt"
+	"math/big"
+	"os"
 )
 
 var (
-    addition       = flag.Bool("a", false, "Perform addition")
-    multiplication = flag.Bool("m", false, "Perform multiplication")
-    power          = flag.Bool("p", false, "Perform exponentiation")
-    factorial      = flag.Bool("f", false, "Calculate factorial")
-    fibonacci      = flag.Bool("F", false, "Calculate fibonacci sequence")
-    startStr       = flag.String("s", "1", "Start value")
-    endStr         = flag.String("e", "100", "End value")
-    hexResult      = flag.Bool("h", false, "Show hexadecimal result")
-    onlyResult     = flag.Bool("r", false, "Show only the result")
-    valueChars     = flag.Bool("v", false, "Show amount of characters in decimal and hexadecimal")
+	addition       = flag.Bool("a", false, "Perform addition")
+	multiplication = flag.Bool("m", false, "Perform multiplication")
+	power          = flag.Bool("p", false, "Perform exponentiation")
+	factorial      = flag.Bool("f", false, "Calculate factorial")
+	fibonacci      = flag.Bool("F", false, "Calculate fibonacci sequence")
+	startStr       = flag.String("s", "1", "Start value")
+	endStr         = flag.String("e", "100", "End value")
+	hexResult      = flag.Bool("h", false, "Show hexadecimal result")
+	onlyResult     = flag.Bool("r", false, "Show only the result")
+	valueChars     = flag.Bool("v", false, "Show amount of characters in decimal and hexadecimal")
 )
 
 func main() {
-    flag.Parse()
+	flag.Parse()
 
-    if flag.NFlag() == 0 {
-        fmt.Println("Usage: steps [flags]")
-        fmt.Println("Flags:")
-        flag.PrintDefaults()
-        os.Exit(1)
-    }
+	if flag.NFlag() == 0 {
+		fmt.Println("Usage: steps [flags]")
+		fmt.Println("Flags:")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 
-    startVal, ok := new(big.Int).SetString(*startStr, 10)
-    if !ok {
-        fmt.Println("Error: Invalid start value.")
-        return
-    }
+	startVal, ok := new(big.Int).SetString(*startStr, 10)
+	if !ok {
+		fmt.Println("Error: Invalid start value.")
+		return
+	}
 
-    endVal, ok := new(big.Int).SetString(*endStr, 10)
-    if !ok {
-        fmt.Println("Error: Invalid end value.")
-        return
-    }
+	endVal, ok := new(big.Int).SetString(*endStr, 10)
+	if !ok {
+		fmt.Println("Error: Invalid end value.")
+		return
+	}
 
-    // Check if start value is greater than end value
-    if startVal.Cmp(endVal) > 0 {
-        fmt.Println("Error: Start value cannot be greater than end value.")
-        return
-    }
+	// Check if start value is greater than end value
+	if startVal.Cmp(endVal) > 0 {
+		fmt.Println("Error: Start value cannot be greater than end value.")
+		return
+	}
 
-    // Use a reasonable maximum for the end value
-    maxEndVal := new(big.Int).Exp(big.NewInt(10), big.NewInt(1000), nil)
+	// Use a reasonable maximum for the end value
+	maxEndVal := new(big.Int).Exp(big.NewInt(10), big.NewInt(1000), nil)
 
-    // Set the end value to the maximum if it exceeds the maximum
-    if endVal.Cmp(maxEndVal) > 0 {
-        endVal.Set(maxEndVal)
-        fmt.Printf("Warning: End value exceeds maximum allowed. Setting end value to %s.\n", maxEndVal.String())
-    }
+	// Set the end value to the maximum if it exceeds the maximum
+	if endVal.Cmp(maxEndVal) > 0 {
+		endVal.Set(maxEndVal)
+		fmt.Printf("Warning: End value exceeds maximum allowed. Setting end value to %s.\n", maxEndVal.String())
+	}
 
-    // Calculate from start value to end value
-    for i := new(big.Int).Set(startVal); i.Cmp(endVal) <= 0; i.Add(i, big.NewInt(1)) {
-        var result big.Int
-        switch {
-        case *addition:
-            result.Add(i, i)
-        case *multiplication:
-            result.Mul(i, i)
-        case *power:
-            result.Exp(i, i, nil)
-        case *factorial:
-            result.MulRange(1, int64(i.Int64()))
-        case *fibonacci:
-            result.Set(fib(i.Int64()))
-        }
+	// Calculate from start value to end value
+	for i := new(big.Int).Set(startVal); i.Cmp(endVal) <= 0; i.Add(i, big.NewInt(1)) {
+		var result big.Int
+		switch {
+		case *addition:
+			result.Add(i, i)
+		case *multiplication:
+			result.Mul(i, i)
+		case *power:
+			result.Exp(i, i, nil)
+		case *factorial:
+			result.MulRange(1, int64(i.Int64()))
+		case *fibonacci:
+			result.Set(fib(i.Int64()))
+		}
 
-        if *onlyResult && *hexResult {
-            fmt.Printf("%X\n", &result)
-            continue
-        }
+		// Print only the result if the flag is set
+		if *onlyResult {
+			printResult(i, &result)
+			fmt.Println()
+			continue
+		}
 
-        if *onlyResult {
-            fmt.Println(result.String())
-            continue
-        }
+		// Otherwise, print the full expression
+		printExpression(i, &result)
+	}
+}
 
-        if *hexResult {
-            hexResult := fmt.Sprintf("%X", &result)
-            fmt.Printf("%s", i)
+func printResult(input, result *big.Int) {
+	if *hexResult {
+		fmt.Printf("%X", result)
+	} else {
+		fmt.Print(result.String())
+	}
+}
 
-            switch {
-            case *addition:
-                fmt.Print(" + ")
-            case *multiplication:
-                fmt.Print(" * ")
-            case *power:
-                fmt.Print("^")
-            case *factorial:
-                fmt.Print("!")
-            case *fibonacci:
-                fmt.Print(" Fibonacci")
-            }
+func printExpression(input, result *big.Int) {
+	if *onlyResult {
+		fmt.Printf("%s = ", input)
+	}
 
-            fmt.Printf("%s = %s", i, hexResult)
-            if *valueChars {
-                fmt.Printf(" (%d characters)", len(hexResult))
-            }
-        } else {
-            fmt.Printf("%s", i)
+	switch {
+	case *addition:
+		fmt.Printf("%s + %s", input, input)
+	case *multiplication:
+		fmt.Printf("%s * %s", input, input)
+	case *power:
+		fmt.Printf("%s ^ %s", input, input)
+	case *factorial:
+		fmt.Printf("%s!", input)
+	case *fibonacci:
+		fmt.Printf("F%s", input)
+	}
 
-            switch {
-            case *addition:
-                fmt.Print(" + ")
-            case *multiplication:
-                fmt.Print(" * ")
-            case *power:
-                fmt.Print("^")
-            case *factorial:
-                fmt.Print("!")
-            case *fibonacci:
-                fmt.Print(" Fibonacci")
-            }
+	fmt.Print(" = ")
+	printResult(input, result)
 
-            fmt.Printf("%s = %s", i, result.String())
-
-            if *valueChars {
-                fmt.Printf(" (%d digits)", len(result.String()))
-            }
-        }
-
-        fmt.Println()
-    }
+	if *valueChars {
+		if *hexResult {
+			fmt.Printf(" (%d characters)\n", len(fmt.Sprintf("%X", result)))
+		} else {
+			fmt.Printf(" (%d digits)\n", len(result.String()))
+		}
+	} else {
+		fmt.Println()
+	}
 }
 
 func fib(n int64) *big.Int {
-    fibNumbers := [2]*big.Int{big.NewInt(0), big.NewInt(1)}
-    for i := int64(0); i < n; i++ {
-        fibNumbers[i%2] = new(big.Int).Add(fibNumbers[0], fibNumbers[1])
-    }
-    return fibNumbers[n%2]
+	fibNumbers := [2]*big.Int{big.NewInt(0), big.NewInt(1)}
+	for i := int64(0); i < n; i++ {
+		fibNumbers[i%2] = new(big.Int).Add(fibNumbers[0], fibNumbers[1])
+	}
+	return fibNumbers[n%2]
 }
 
